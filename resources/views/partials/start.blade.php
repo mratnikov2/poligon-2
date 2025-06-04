46c14ea46c99d1639f9b73fedd705c02369c86fa
mratnikov2 <mratnikov@vu.cdu.edu.ua>
2025-06-04 17:08:00 +0300

    1-st attempt

---
+++ resources/views/partials/start.blade.php
@@ -0,0 +1,66 @@
+<html>
+<div class="w-full max-w-[1280px] h-[500px] bg-blue-400 mx-auto rounded-2xl flex items-center justify-center mt-10">
+    <div class="text-2xl text-center text-white">
+        Хорошо меня видно?
+    </div>
+</div>
+<div class="w-full max-w-[1280px] h-[600px] mx-auto mt-10 flex flex-row">
+    <div class="w-full max-w-[426px] h-[600px] bg-sky-400 rounded-2xl mx-4">
+        <h1 class="text-2xl font-bold text-center text-white mt-5">
+            Я считаю что росту!
+        </h1>
+        <p class="text-2xl mt-5 text-center text-white">
+            Если мне больно,<br>
+            то я над собой расту — <br>
+            жизнь растекается в корни, <br>
+            но смотрит вверх. <br>
+        </p>
+        <img src=""
+             alt=""
+             class="w-64 h-64 bg-white rounded-full mx-auto mt-5" />
+        <button class="w-[40px] h-[30px] bg-orange-400 mt-10 mx-auto border-2 border-black rounded text-center flex
+            items-center justify-center"
+        >
+                Жми
+        </button>
+    </div>
+    <div class=" w-full max-w-[426px] h-[600px] bg-sky-400 rounded-2xl mx-4">
+        <h1 class="text-2xl font-bold text-center text-white mt-5">
+            И это факт!
+        </h1>
+        <p class="text-2xl mt-5 text-center text-white">
+            Взгляд в себя, <br>
+            в поток идей...<br>
+            Как рентген, <br>
+            порою служит<br>
+        </p>
+        <img src=""
+             alt=""
+             class="w-64 h-64 bg-white rounded-full mx-auto mt-5" />
+        <button class="w-[40px] h-[30px] bg-orange-400 mt-10 mx-auto border-2 border-black rounded text-center flex
+            items-center justify-center"
+        >
+            Жми
+        </button>
+    </div>
+    <div class=" w-full max-w-[426px] h-[600px] bg-sky-400 rounded-2xl mx-4">
+        <h1 class="text-2xl font-bold text-center text-white mt-5">
+            Я считаю что росту!
+        </h1>
+        <p class="text-2xl mt-5 text-center text-white">
+            Если мне больно,<br>
+            то я над собой расту — <br>
+            жизнь растекается в корни, <br>
+            но смотрит вверх. <br>
+        </p>
+        <img src=""
+             alt=""
+             class="w-64 h-64 bg-white rounded-full mx-auto mt-5" />
+        <button class="w-[40px] h-[30px] bg-orange-400 mt-10 mx-auto border-2 border-black rounded text-center flex
+            items-center justify-center"
+        >
+            Жми
+        </button>
+    </div>
+</div>
+</html>
