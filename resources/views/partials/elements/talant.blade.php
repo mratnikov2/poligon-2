157d030267eae45e3b03a2029a11b9f108c61907
mratnikov2 <mratnikov@vu.cdu.edu.ua>
2025-06-12 16:12:52 +0300

    refactor(talant): New aos-implementation (all part)
    I made all in this part
    
    BREAKING CHANGE: I think yes but cant undestand where

--- resources/views/partials/elements/talant.blade.php
+++ resources/views/partials/elements/talant.blade.php
@@ -13,18 +13,18 @@
     </div>
 </div>
 <div class="mt-20 w-full max-w-[2560px] bg-white h-[1000px]">
-    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black">
+    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black" data-aos="zoom-in">
         <h1 class="text-3xl text-left">
             About CAA Baseball
         </h1>
     </div>
     <div class="w-full max-w-[1280px] mx-auto mt-5 flex flex-row">
-        <div class="w-1/3">
+        <div class="w-1/3" data-aos="fade-left">
             <p class="text-red-700 text-5xl font-bold leading-tight">
                 America’s <br> greatest <br> pastime,<br> powered by <br> CAA.
             </p>
         </div>
-        <div class="w-2/3">
+        <div class="w-2/3" data-aos="fade-right">
             <p class="text-left text-2xl leading-tight mt-2">
                 The CAA Baseball team of player representatives and draft advisors has <br>
                 more than 250 years of combined industry experience and is second <br>
@@ -36,7 +36,7 @@
             </p>
         </div>
     </div>
-    <div class="w-full max-w-[2560px] bg-red-700 h-[50%] mt-5">
+    <div class="w-full max-w-[2560px] bg-red-700 h-[50%] mt-5" data-aos="zoom-in">
         <p class="text-center text-7xl text-white font-bold p-10">
             “CAA agents leverage information <br>
             as good as any in the business, <br>
@@ -44,31 +44,31 @@
             relationships with club executives <br>
             that really sets them apart.”
         </p>
-        <p class="text-white text-center text-1xl">
+        <p class="text-white text-center text-1xl" data-aos="zoom-in">
             ESPN.com
         </p>
     </div>
 </div>
 <div class="mt-5 w-full max-w-[2560px] bg-white h-[1800px]">
-    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black">
+    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black" data-aos="zoom-in">
         <h1 class="text-3xl text-left mb-2">
             Baseball by the Numbers
         </h1>
     </div>
     <div class="w-full max-w-[1280px] mx-auto mt-5 flex flex-row">
-        <div class="w-1/3">
+        <div class="w-1/3" data-aos="fade-right">
             <p class="text-red-700 text-[180px] font-bold leading-tight">
                 132
             </p>
         </div>
-        <div class="w-2/3">
+        <div class="w-2/3" data-aos="fade-left">
             <p class="text-left text-6xl leading-tight mt-12 font-bold">
                 All-Star apperenceas by CAA clients
             </p>
         </div>
     </div>
     <div class="w-full max-w-[1280px] mx-auto mt-20 border-t border-t-black flex flex-row">
-        <div class="w-1/3">
+        <div class="w-1/3" data-aos="fade-right">
             <div class="flex items-center h-[200px]">
                 <p class="text-left text-[30px] text-black leading-tight font-bold">
                     Our clients earn the game's <br>
@@ -78,32 +78,32 @@
         </div>
         <!-- правая колонка -->
         <div class="w-2/3 flex flex-col justify-between">
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold ">
                     <span class="text-red-500">6</span> MVP Awards
                 </p>
             </div>
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold">
                     <span class="text-red-500">6</span> Cy Young Awards
                 </p>
             </div>
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold">
                     <span class="text-red-500">8</span> Rookie of the Year Awards
                 </p>
             </div>
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold">
                     <span class="text-red-500">33</span> Silver Sluggers
                 </p>
             </div>
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold">
                     <span class="text-red-500">15</span> First-Team All MLB
                 </p>
             </div>
-            <div class="flex items-center h-[200px] border-b border-b-black">
+            <div class="flex items-center h-[200px] border-b border-b-black" data-aos="fade-left">
                 <p class="text-left text-black text-[30px] leading-tight font-bold">
                     <span class="text-red-500">32</span> Gold Gloves
                 </p>
@@ -114,48 +114,48 @@
 <div class="w-full max-w-[2560px] h-[1000px] bg-gray-600 mb-10">
     <div class="flex flex-col">
         <!-- Заголовок -->
-        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10" data-aos="zoom-in">
             <p class="text-gray-300 text-left text-xl">
                 Our Draft Expertise
             </p>
         </div>
         <!-- Блок с картинкой и текстом -->
         <div class="flex w-full max-w-[1280px] mx-auto flex-row">
-            <div class="w-1/3">
+            <div class="w-1/3" data-aos="zoom-in">
                 <img
                         src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
                         alt="Image"
                         class="w-full h-auto p-4">
             </div>
             <div class="w-2/3 p-4 text-gray-100 space-y-4 mb-10">
-                <h2 class="text-left text-7xl font-bold">
+                <h2 class="text-left text-7xl font-bold" data-aos="zoom-in">
                     Elite advisement for elite talent.
                 </h2>
-                <p class="text-xl">
+                <p class="text-xl" data-aos="zoom-in">
                     Major League Baseball's Rule 4 Amateur Draft is the main entry point into <br>
                     the game today. CAA's history of superior service and education with <br>
                     families has redefined the role of the draft advisor, and the results speak for <br>
                     themselves.
                 </p>
-                <p class="text-xl">
+                <p class="text-xl" data-aos="zoom-in">
                     The MLB Draft is an art, not a science, and in the draft pool system, CAA's <br>
                     average first-round advisee has signed for more dollars above slot than <br>
                     players represented by any other firm.
                 </p>
             </div>
         </div>
-        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10" data-aos="zoom-in">
             <p class="text-gray-300 text-left text-xl">
                 Draft Expertise by the Numbers
             </p>
         </div>
         <div class="flex w-full max-w-[1280px] mx-auto flex-row items-center">
             <div class="w-1/3">
-                <p class="text-red-700 text-[150px] text-center font-bold">
+                <p class="text-red-700 text-[150px] text-center font-bold" data-aos="zoom-in">
                     72
                 </p>
             </div>
-            <div class="w-2/3 p-4 text-gray-100 mb-10 mt-10">
+            <div class="w-2/3 p-4 text-gray-100 mb-10 mt-10" data-aos="zoom-in">
                 <h2 class="text-left text-5xl font-bold">
                     first-round selection since formation
                 </h2>
@@ -166,24 +166,24 @@
 <div class="w-full max-w-[2560px] h-[1000px] bg-white mb-10">
     <div class="flex flex-col">
         <!-- Заголовок -->
-        <div class="w-full max-w-[1280px] mx-auto border-b border-b-gray-700 py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-b-gray-700 py-10" data-aos="zoom-in">
             <p class="text-gray-700 text-left text-xl">
                 Our Player Development Expertise
             </p>
         </div>
         <!-- Блок с картинкой и текстом -->
         <div class="flex w-full max-w-[1280px] mx-auto flex-row">
-            <div class="w-1/3">
+            <div class="w-1/3" data-aos="zoom-in">
                 <img
                         src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
                         alt="Image"
                         class="w-full h-auto p-4">
             </div>
             <div class="w-2/3 p-4 text-gray-700 space-y-4 mb-10">
-                <h2 class="text-left text-7xl font-bold">
+                <h2 class="text-left text-7xl font-bold" data-aos="zoom-in">
                     Development by design.
                 </h2>
-                <p class="text-xl">
+                <p class="text-xl" data-aos="zoom-in">
                     CAA represents a large number of the game's top prospects and tomorrow's MLB stars.<br>
                     CAA's in-house development staff is second-to-none in their ability to help produce <br>
                     CAA represents a large number of the game's top prospects and tomorrow's MLB stars. <br>
@@ -193,18 +193,18 @@
                 </p>
             </div>
         </div>
-        <div class="w-full max-w-[1280px] mx-auto border-b border-gray-700 py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-gray-700 py-10" data-aos="zoom-in">
             <p class="text-gray-700 text-left text-xl">
                 Development by the Numbers
             </p>
         </div>
         <div class="flex w-full max-w-[1280px] mx-auto flex-row items-center">
-            <div class="w-1/3">
+            <div class="w-1/3" data-aos="zoom-in">
                 <p class="text-red-700 text-[150px] text-center font-bold">
                     67
                 </p>
             </div>
-            <div class="w-2/3 p-4 text-gray-700 mb-10 mt-10">
+            <div class="w-2/3 p-4 text-gray-700 mb-10 mt-10" data-aos="zoom-in">
                 <h2 class="text-left text-5xl font-bold">
                     MLB Futures Game Participiants since formations
                 </h2>
@@ -215,24 +215,24 @@
 <div class="w-full max-w-[2560px] h-[1000px] bg-gray-600 mb-10">
     <div class="flex flex-col">
         <!-- Заголовок -->
-        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10" data-aos="zoom-in">
             <p class="text-gray-300 text-left text-xl">
                 MLB Contract Expertise
             </p>
         </div>
         <!-- Блок с картинкой и текстом -->
         <div class="flex w-full max-w-[1280px] mx-auto flex-row">
-            <div class="w-1/3">
+            <div class="w-1/3" data-aos="zoom-in">
                 <img
                         src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=="
                         alt="Image"
                         class="w-full h-auto p-4">
             </div>
             <div class="w-2/3 p-4 text-gray-100 space-y-4 mb-10">
-                <h2 class="text-left text-7xl font-bold">
+                <h2 class="text-left text-7xl font-bold" data-aos="zoom-in">
                     Maximizing value while meeting client goals.
                 </h2>
-                <p class="text-xl">
+                <p class="text-xl" data-aos="zoom-in">
                     With strong, trusted relationships across the league, CAA agents <br>
                     have negotiated some of the game's largest, record-breaking deals. <br>
                     Using industry-leading research and analysis capabilities, proprietary <br>
@@ -243,18 +243,18 @@
 
             </div>
         </div>
-        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10">
+        <div class="w-full max-w-[1280px] mx-auto border-b border-b-white py-10" data-aos="zoom-in">
             <p class="text-gray-300 text-left text-xl">
                 Contract Expertise by the Numbers
             </p>
         </div>
         <div class="flex w-full max-w-[1280px] mx-auto flex-row items-center">
-            <div class="w-1/3">
+            <div class="w-1/3" data-aos="zoom-in">
                 <p class="text-red-700 text-[150px] text-center font-bold">
                     21
                 </p>
             </div>
-            <div class="w-2/3 p-4 text-gray-100 mb-10 mt-10">
+            <div class="w-2/3 p-4 text-gray-100 mb-10 mt-10" data-aos="zoom-in">
                 <h2 class="text-left text-5xl font-bold">
                     contracts of 50$mln+ since the begining of 2011 seasons
                 </h2>
@@ -263,18 +263,18 @@
     </div>
 </div>
 <div class="mt-5 w-full max-w-[2560px] bg-white h-[1200px]">
-    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black">
+    <div class="w-full max-w-[1280px] mx-auto border-b border-b-black" data-aos="zoom-in">
         <h1 class="text-3xl text-left mb-2">
             Brand Building
         </h1>
     </div>
     <div class="w-full max-w-[1280px] mx-auto mt-5 flex flex-row">
-        <div class="w-1/3">
+        <div class="w-1/3" data-aos="fade-left">
             <p class="text-red-700 text-[50px] font-bold leading-tight mt-10">
                 A 360-degree approach to our clients' ambitions.
             </p>
         </div>
-        <div class="w-2/3">
+        <div class="w-2/3" data-aos="fade-right">
             <p class="text-left text-4xl leading-tight mt-12 font-bold">
                 Marketing & Endorsements
             </p>
